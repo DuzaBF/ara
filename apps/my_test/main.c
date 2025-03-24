6609d3bcdc33d486ee186ea7c89a273e56024e5a
Airat Galiamov <DuzaBF@users.noreply.github.com>
2025-03-24 19:34:49 +0100

    Add more control over placement

--- apps/my_test/main.c
+++ apps/my_test/main.c
@@ -12,19 +12,24 @@
 
 #include "foo.h"
 
+static my_array_t a __attribute__((section(".my_data_a")));
+static my_array_t b __attribute__((section(".my_data_b")));
+
 int main()
 {
-    my_array_t a, b;
+    printf("&a = %#010X\n", (uint32_t)&a.data);
+    printf("&b = %#010X\n", (uint32_t)&b.data);
     for (int i = 0; i < MY_ARRAY_SIZE; ++i)
     {
         a.data[i] = (((uint32_t)i) << 24) | (((uint32_t)i) << 16) | (((uint32_t)i) << 8) | (((uint32_t)i) << 0);
+        // a.data[3] = 0x03030303
     }
     bar(&a, &b);
-    // test_vsetvl();
-    // foo_intr(&a, &b, &c);
-    for (int i = 10; i < 11; ++i) {
+    for (int i = 0; i < 25; ++i)
+    {
         printf("a[%d] = %#010X; ", i, a.data[i]);
         printf("b[%d] = %#010X; ", i, b.data[i]);
+        printf("\n");
     }
     printf("\n");
 }
